4bea392081f826183df306eacded4312b931d109
xin.zhang <xin.b.zhang@oracle.com>
2022-10-27 21:54:23 +0800

    update

--- chapter02/sql/db.sql
+++ chapter02/sql/db.sql
@@ -1,5 +1,14 @@
 CREATE TABLE `region` (
+                          `id` varchar(100) NOT NULL,
                           `region_id` varchar(100) NOT NULL,
-                          `region_name` varchar(200) DEFAULT NULL,
-                          PRIMARY KEY (`region_id`)
+                          `region_name` varchar(200) NOT NULL,
+                          PRIMARY KEY (`id`)
 );
+
+INSERT INTO `region` (`id`, `region_id`, `region_name`) VALUES ("ap-melbourne-1", "ap-melbourne-1", "オーストラリア南東部(メルボルン)");
+INSERT INTO `region` (`id`, `region_id`, `region_name`) VALUES ("ap-osaka-1", "ap-osaka-1", "日本中央部(大阪)");
+INSERT INTO `region` (`id`, `region_id`, `region_name`) VALUES ("ap-sydney-1", "ap-sydney-1", "オーストラリア東部(シドニー)");
+INSERT INTO `region` (`id`, `region_id`, `region_name`) VALUES ("ap-tokyo-1", "ap-tokyo-1", "日本東部(東京)");
+INSERT INTO `region` (`id`, `region_id`, `region_name`) VALUES ("sa-saopaulo-1", "sa-saopaulo-1", "ブラジル東部(サンパウロ)");
+
+COMMIT;
