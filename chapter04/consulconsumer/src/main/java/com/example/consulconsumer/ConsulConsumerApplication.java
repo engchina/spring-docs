f3aca3f4aab15ed27d44aac3488f0504aa42157e
xin.zhang <xin.b.zhang@oracle.com>
2022-11-06 12:03:30 +0800

    update

--- chapter04/consulconsumer/src/main/java/com/example/consulconsumer/ConsulConsumerApplication.java
+++ chapter04/consulconsumer/src/main/java/com/example/consulconsumer/ConsulConsumerApplication.java
@@ -3,11 +3,10 @@ package com.example.consulconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class ConsulConsumerApplication {
 
 	public static void main(String[] args) {
--- chapter04/consulconsumer/src/main/java/com/example/consulconsumer/controller/ConsumerController.java
+++ chapter04/consulconsumer/src/main/java/com/example/consulconsumer/controller/ConsumerController.java
@@ -1,20 +1,26 @@
 package com.example.consulconsumer.controller;
 
-import com.example.consulconsumer.service.MyFeignClient;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
 
 
 @RestController
 public class ConsumerController {
 
     @Resource
-    MyFeignClient myFeignClient;
+    private LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/hello")
     public String hello() {
-        return myFeignClient.hello();
+        ServiceInstance serviceInstance = loadBalancerClient.choose("service-provider");
+        URI uri = serviceInstance.getUri();
+        String callService = new RestTemplate().getForObject(uri + "/hello", String.class);
+        return callService;
     }
 }
